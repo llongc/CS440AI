a9b12a68c8cd7189a855e126efe520d83ecf0bb0
llongc <cynthia.d.long@outlook.com>
2019-10-12 17:06:11 -0400

    。
    。

--- cell.py
+++ cell.py
@@ -10,8 +10,8 @@ class cell:
 
     # compares the second value
     def __lt__(self, other):
-        return (self.f, self.g) < (other.f, other.g)  #small g value
-        # return self.f * 300 - self.g < other.f * 300 - other.g   #larger g value
+        # return (self.f, self.g) < (other.f, other.g)  #small g value
+        return self.f * 300 - self.g < other.f * 300 - other.g   #larger g value
 
     def getHeuristic(self, targetx, targety):
         self.h = abs(targetx - self.x) + abs(targety - self.y)
